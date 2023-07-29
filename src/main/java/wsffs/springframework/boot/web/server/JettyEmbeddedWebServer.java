d09143bdf9ddef204167d235bb1cb2e5f20a5c4c
hyeyoom <neoul_chw@icloud.com>
2023-07-29 21:30:07 +0900

    feat: DispatcherServlet 구현

--- src/main/java/wsffs/springframework/boot/web/server/JettyEmbeddedWebServer.java
+++ src/main/java/wsffs/springframework/boot/web/server/JettyEmbeddedWebServer.java
@@ -1,6 +1,9 @@
 package wsffs.springframework.boot.web.server;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import wsffs.springframework.web.servlet.DispatcherServlet;
 
 public class JettyEmbeddedWebServer implements WebServer {
 
@@ -9,8 +12,15 @@ public class JettyEmbeddedWebServer implements WebServer {
 
     public JettyEmbeddedWebServer(int port) {
         if (port > 0) {
+            final ServletContextHandler contextHandler = new ServletContextHandler();
+            contextHandler.setContextPath("/");
+            final DispatcherServlet dispatcherServlet = new DispatcherServlet();
+            final ServletHolder dispatcherServletHolder = new ServletHolder(dispatcherServlet);
+            contextHandler.addServlet(dispatcherServletHolder, "/*");
+            final Server server = new Server(port);
+            server.setHandler(contextHandler);
             this.port = port;
-            this.server = new Server(port);
+            this.server = server;
         } else {
             throw new RuntimeException("포트 번호 지정 안함");
         }
